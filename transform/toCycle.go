65305f8d85f1383dcccfb1b132fdbc9ee83fb9e0
egorka <egorka@tut.by>
2019-08-16 16:42:01 +0300

    intermediate

--- transform/toCycle.go
+++ transform/toCycle.go
@@ -5,12 +5,30 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"path"
 
 	pc "github.com/egorka-gh/pixlpark/photocycle"
 	pp "github.com/egorka-gh/pixlpark/pixlpark/service"
 )
 
-var errCantTransform = errors.New("CantTransform")
+//internal errors, factory has to make decision what to do (rise error or proceed transform)
+
+//ErrCantTransform inapplicable transform method
+type ErrCantTransform error
+
+//ErrSourceNotFound source file or folder not found
+type ErrSourceNotFound error
+
+var (
+	errCantTransform = ErrCantTransform(errors.New("Can't transform"))
+)
+
+type fileCopy struct {
+	OldName  string
+	NewName  string
+	Process  bool
+	SheetIdx int
+}
 
 func (fc *Factory) transformAlias(ctx context.Context, item pp.OrderItem, order *pc.Order) error {
 	//try build by alias
@@ -30,6 +48,27 @@ func (fc *Factory) transformAlias(ctx context.Context, item pp.OrderItem, order 
 	switch alias.Type {
 	case 1, 2, 3:
 		//book
+		//get file list
+		basePath := path.Join(fc.wrkFolder, fmt.Sprintf("%d", item.OrderID), item.DirectoryName)
+		list, err := folderList(basePath)
+		if err != nil {
+			return ErrSourceNotFound(err)
+		}
+		if len(list) == 0 {
+			return ErrSourceNotFound(fmt.Errorf("Empty folder '%s'", basePath))
+		}
+
+		//exclude preview
+		/*
+			re := regexp.MustCompile(`(_preview\.)`)
+			fmt.Println(re.MatchString("surface_0_preview.png"))
+		*/
+		//get index
+		/*
+			re := regexp.MustCompile(`^surface_\[(\d+)\]`)
+			m :=re.FindStringSubmatch("surface_[78888](oblozhka)_zone_[0](oblozhka).jpg")
+		*/
+
 		//decode filenames to cycle names '000-00.jpg'
 		//copy to wrk folder/orderid/alias
 		//check books num create copy of some file vs last book num
--- transform/util.go
+++ transform/util.go
@@ -1,10 +1,23 @@
 package transform
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strconv"
 	"strings"
 )
+
+var allowedExt = map[string]bool{
+	".jpg":  true,
+	".jpeg": true,
+	".png":  true,
+	".tif":  true,
+	".tiff": true,
+	".bmp":  true,
+	".gif":  true,
+}
 
 func replaceRootPath(oldPath, root string) string {
 	oldPath = filepath.Clean(oldPath)
@@ -14,4 +27,80 @@ func replaceRootPath(oldPath, root string) string {
 	}
 	ps[0] = root
 	return filepath.Join(ps...)
+}
+
+func folderOpen(path string) (*os.File, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	fi, err := file.Stat()
+	if err != nil {
+		file.Close()
+		return nil, err
+	}
+	if !fi.IsDir() {
+		file.Close()
+		return nil, fmt.Errorf("Path '%s' is not a folder", path)
+	}
+	return file, nil
+}
+
+func folderList(path string) ([]fileCopy, error) {
+
+	f, err := folderOpen(path)
+	if err != nil {
+		return []fileCopy{}, err
+	}
+	defer f.Close()
+
+	list, err := f.Readdir(-1)
+	if err != nil {
+		return []fileCopy{}, err
+	}
+
+	var res = make([]fileCopy, 0, len(list))
+	for _, fi := range list {
+		if !fi.IsDir() {
+			res = append(res, fileCopy{OldName: fi.Name(), Process: allowedExt[filepath.Ext(fi.Name())]})
+		}
+	}
+	return res, nil
+}
+
+func listIndexItems(list []fileCopy, hasCover bool) error {
+	rep, err := regexp.Compile(`(_preview\.)`)
+	if err != nil {
+		return err
+	}
+	//fmt.Println(re.MatchString("surface_0_preview.png"))
+	rei, err := regexp.Compile(`^surface_\[(\d+)\]`)
+	if err != nil {
+		return err
+	}
+	//m :=re.FindStringSubmatch("surface_[78888](oblozhka)_zone_[0](oblozhka).jpg")
+
+	for i, fi := range list {
+		if fi.Process {
+			if rep.MatchString(fi.OldName) {
+				list[i].Process = false
+			} else {
+				sm := rei.FindStringSubmatch(fi.OldName)
+				if len(sm) != 2 {
+					//TODO error?
+					list[i].Process = false
+				} else {
+					idx, err := strconv.Atoi(sm[1])
+					if err != nil {
+						return err
+					}
+					if !hasCover {
+						idx++
+					}
+					list[i].SheetIdx = idx
+				}
+			}
+		}
+	}
+	return nil
 }
